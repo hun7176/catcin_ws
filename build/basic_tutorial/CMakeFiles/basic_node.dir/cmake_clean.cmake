a717e67f11af8ba25904864229d2cb507025e1de
hun7176 <hun7176@naver.com>
2023-07-21 17:22:07 +0900

    first git

---
+++ build/basic_tutorial/CMakeFiles/basic_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/seungheon/catkin_ws/devel/lib/basic_tutorial/basic_node"
+  "/home/seungheon/catkin_ws/devel/lib/basic_tutorial/basic_node.pdb"
+  "CMakeFiles/basic_node.dir/src/basic.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/basic_tutorial/src/basic.cpp
@@ -0,0 +1,16 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "basic_node");
+    ros::NodeHandle n;
+    ros::Rate loop_rate(60);
+
+    while(ros::ok())
+    {
+        ROS_INFO("Hello world!");
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
